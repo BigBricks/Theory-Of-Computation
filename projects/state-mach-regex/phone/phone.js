ff444cb7d75bb7b70612063d7b870d67f41ef926
unknown <bsa6.23.94@gmail.com>
2018-07-18 17:56:07 -0400

    phone regex done i think

--- projects/state-mach-regex/phone/phone.js
+++ projects/state-mach-regex/phone/phone.js
@@ -12,12 +12,26 @@ var rl = readline.createInterface({
 rl.on('line', function (line) {
 
     // !!!! IMPLEMENT ME
-
+    // http://www.ntu.edu.sg/home/ehchua/programming/howto/regexe.html
+    // Good Website for the whole of regex but doesnt explain your current code like the below
+    // https://regexr.com/
+    // Thats the best website for understanding regex
     // Come up with the phone regex
+    //Basically the first d / s block is checking for a digit set of 3
+    //It also checks if it is enclosed by a set of parenthesis
+    // Set 2 is the same but instead of parethensis it checks for hyphens
+    //Set 3 is just the final four digits
+    const phoneRegex = /\(?(\d{3})\)?[\-|\s]?(\d{3})[\-|\s]?(\d{4})/; 
 
     // Find matches
+    const matches = line.match(phoneRegex);
 
     // If match found, print number with no spaces, parens, or dashes
-
+    if(matches){
+      console.log(matches);
+    }
+    else {
+      console.log("Hah what number")
+    }
     // Else print that no number was found
 });
