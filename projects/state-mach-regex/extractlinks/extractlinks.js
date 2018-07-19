c4e43e2d33b821169b96981d818c8bc13a9a94fd
unknown <bsa6.23.94@gmail.com>
2018-07-19 17:45:22 -0400

    extract links and phone done

--- projects/state-mach-regex/extractlinks/extractlinks.js
+++ projects/state-mach-regex/extractlinks/extractlinks.js
@@ -12,9 +12,14 @@ const filename = args[0];
 // !!!! IMPLEMENT ME
 
 // Read file
+const fileEE = fs.readFileSync(filename, "utf8");
 
 // Set up regex
-
+const linkRegex = /(http|https)?:\/\/[^\\'">\s]+/ig;
+// const all = new RegExp(linkRegex);
 // Find matches
-
+// const read = all.exec(file);
+const matches = fileEE.match(linkRegex);
 // Print all matches
+console.log(matches);
+
